772ee4be336b3d7e1682ec829573ca13d0b1b016
edulourenzo <edulourenzo@hotmail.com>
2024-10-25 19:29:29 -0300

    Adicionar classes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Fulano da Silva");
+
+        Conta cc = new ContaCorrente(cliente);
+        Conta poupanca = new ContaPoupanca(cliente);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirInfosComuns();
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
     }
+
 }
